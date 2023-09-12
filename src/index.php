a44fe6e7a332e2a884a8a47fde87262cf88dd013
rym49 <hatanakaryoma.130@gmail.com>
2023-09-12 22:22:01 +0900

    project ready

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+
+// urlをyamlから読み込む
+
+// fetch
+
+// 更新差分のみ抽出
+
+// slackに通知する
---
+++ test/indexTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class IndexTest extends TestCase
+{
+  public function test(): void
+  {
+  }
+}
