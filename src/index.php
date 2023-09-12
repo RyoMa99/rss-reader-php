9d558398749902f854c88bd155b4365302b89e17
rym49 <hatanakaryoma.130@gmail.com>
2023-09-12 22:45:56 +0900

    yaml読み込みの追加

--- src/index.php
+++ src/index.php
@@ -2,8 +2,19 @@
 
 declare(strict_types=1);
 
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
 
 // urlをyamlから読み込む
+try {
+  $urlFilePath = getenv('URL_FILE_PATH') ?: '../url.yaml';
+  $value = Yaml::parseFile($urlFilePath);
+  var_dump($value);
+} catch (ParseException $e) {
+  echo $e->getMessage();
+}
 
 // fetch
 
