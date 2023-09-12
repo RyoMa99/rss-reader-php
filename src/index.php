e2d89754285cce19f584e222adc81c4c0f94f65a
rym49 <hatanakaryoma.130@gmail.com>
2023-09-12 23:32:33 +0900

    yaml読み出しを関数化

--- src/index.php
+++ src/index.php
@@ -13,18 +13,27 @@ use Symfony\Component\Yaml\Yaml;
 $logger = new Logger(__FILE__);
 $logger->pushHandler(new StreamHandler('php://stdout', Level::Debug));
 
-// urlをyamlから読み込む
-try {
-  $urlFilePath = getenv('URL_FILE_PATH') ?: '../url.yaml';
-  $value = Yaml::parseFile($urlFilePath);
-  var_dump($value);
-} catch (ParseException $e) {
-  $logger->error($e->getMessage());
-  exit(1);
-}
-
+$urlFilePath = getenv('URL_FILE_PATH') ?: '../url.yaml';
+var_dump(importYamlFile($urlFilePath));
 // fetch
-
 // 更新差分のみ抽出
-
 // slackに通知する
+$logger->info("fin");
+
+
+/**
+ * 引数に与えられたパスのyamlファイルを読み込みその中身を返す
+ *
+ * @param string $filePath
+ * @return array{name: string, url: string}[]
+ */
+function importYamlFile(string $filePath): array
+{
+  try {
+    return Yaml::parseFile($filePath);
+  } catch (ParseException $e) {
+    global $logger;
+    $logger->error($e->getMessage());
+    exit(1);
+  }
+}
