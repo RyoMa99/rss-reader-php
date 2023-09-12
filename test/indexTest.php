136d18b398bbc69ecf3032d495472ce1b7446d69
rym49 <hatanakaryoma.130@gmail.com>
2023-09-13 00:24:24 +0900

    (途中)yaml読み込みのテスト追加
    グローバル変数が悪さしてる

--- test/indexTest.php
+++ test/indexTest.php
@@ -2,11 +2,27 @@
 
 declare(strict_types=1);
 
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
 
 class IndexTest extends TestCase
 {
+  private string $yamlFileName = 'test.yaml';
+
+  public function setUp(): void
+  {
+    vfsStream::newFile($this->yamlFileName)
+      ->withContent(<<<EOF
+- name: JSer.info
+url: https://jser.info/rss/
+- name: 株式会社メドレー
+url: https://developer.medley.jp/rss.xml
+EOF)
+      ->at(vfsStream::setup('.'));
+  }
+
   public function test(): void
   {
+    importYamlFile("test");
   }
 }
